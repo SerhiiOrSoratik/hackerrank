ec2b47270b0cf1dd67f28ec577783b185c5e00be
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-07-27 18:01:08 +0300

    test solution

--- leaderboard.js
+++ leaderboard.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,63 +32,51 @@ function readLine() {
  */
 
 export function climbingLeaderboard(ranked, player) {
-    if(ranked.length === 0 && player.length === 0) {
-        return [];
+    // Унікальні значення
+let rank = [];
+  for (let i = 0; i < ranked.length; i++) {
+    if (rank.length === 0) {
+      rank.push(ranked[i]);
+    } else {
+      if (rank[rank.length - 1] === ranked[i]) {
+        continue;
+      }
+      rank.push(ranked[i]);
     }
-    else if(ranked.length !== 0 && player.length === 0) {
-        return [];
+  }
+  console.log(rank)
+  let res = [];
+  let lowestRank = rank.length;
+  for (let i = 0; i < player.length; i++) {
+    while (player[i] >= rank[lowestRank - 1] && lowestRank > 0) {
+      rank.pop;
+      lowestRank--;
     }
-    else if(ranked.length === 0 && player.length !== 0) {
-        let result = [];
-        for (let i = 0; i < player.length; i++) {
-            result.push(1);
-        }
-        return result;
-    }
-    else {
-        let result = [];
-        ranked = ranked.filter(function(item, pos) {
-            return ranked.indexOf(item) == pos;
-        })
-        player.map(playerScore => {
-            if (ranked.indexOf(playerScore) !== -1) {
-                result.push(ranked.indexOf(playerScore) + 1);
-            }
-            else {
-                if (playerScore < ranked[ranked.length - 1]) {
-                    result.push(ranked.length + 1);
-                }
-                else {
-                    for(let i = 0; i < ranked.length; i++) {
-                      if (playerScore > ranked[i]) {
-                            result.push(i + 1);
-                            break;
-                       }
-                       else {
-                             continue;
-                         }
-                     }
-                }
-            }
-        });
-        return result;
-    }
+    res.push(lowestRank + 1);
+  }
+  return res;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const rankedCount = parseInt(readLine().trim(), 10);
+  const rankedCount = parseInt(readLine().trim(), 10);
 
-    const ranked = readLine().replace(/\s+$/g, '').split(' ').map(rankedTemp => parseInt(rankedTemp, 10));
+  const ranked = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((rankedTemp) => parseInt(rankedTemp, 10));
 
-    const playerCount = parseInt(readLine().trim(), 10);
+  const playerCount = parseInt(readLine().trim(), 10);
 
-    const player = readLine().replace(/\s+$/g, '').split(' ').map(playerTemp => parseInt(playerTemp, 10));
+  const player = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((playerTemp) => parseInt(playerTemp, 10));
 
-    const result = climbingLeaderboard(ranked, player);
+  const result = climbingLeaderboard(ranked, player);
 
-    ws.write(result.join('\n') + '\n');
+  ws.write(result.join("\n") + "\n");
 
-    ws.end();
+  ws.end();
 }
